24bdb38658e6b4d1d95deecca8b5cceae442bffa
colton456p <colton.p@hotmail.com>
2024-11-07 12:06:32 -0800

    fixed errors + linter

--- src/code_parser.py
+++ src/code_parser.py
@@ -2,8 +2,6 @@ import re
 
 import pandas as pd
 from bs4 import BeautifulSoup
-from fpdf import FPDF
-from jinja2 import Template
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer
@@ -14,203 +12,196 @@ def parse_code():
 
     pre_LLM = {
         "Dec-18": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Jan-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Feb-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Mar-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Apr-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "May-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Jun-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Jul-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Aug-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Sep-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Oct-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Nov-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Dec-19": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Jan-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Feb-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Mar-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Apr-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "May-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Jun-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Jul-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Aug-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
         "Sep-20": {
-                "question":[],
-                "answer_score":[],
-                "answer":[],
-                "question_tags":[],
-            },
+            "question": [],
+            "answer_score": [],
+            "answer": [],
+            "question_tags": [],
+        },
     }
-    
+
     for index, row in df.iterrows():
         if row.iloc[0] in pre_LLM:
-            pre_LLM[row.iloc[0]]['question'].append(row.iloc[2])
-            pre_LLM[row.iloc[0]]['answer_score'].append(row.iloc[3])
-            pre_LLM[row.iloc[0]]['answer'].append(row.iloc[4])
-            pre_LLM[row.iloc[0]]['question_tags'].append(row.iloc[5])
+            pre_LLM[row.iloc[0]]["question"].append(row.iloc[2])
+            pre_LLM[row.iloc[0]]["answer_score"].append(row.iloc[3])
+            pre_LLM[row.iloc[0]]["answer"].append(row.iloc[4])
+            pre_LLM[row.iloc[0]]["question_tags"].append(row.iloc[5])
     generate_pdf_report(pre_LLM)
 
-    
-    
+
 def generate_pdf_report(data_dict):
     doc = SimpleDocTemplate("pre_llm_report.pdf", pagesize=letter)
     styles = getSampleStyleSheet()
     story = []
 
     for month, details in data_dict.items():
-        story.append(Paragraph(f"<b>Month: {month}</b>", styles['Heading2']))
-        
-        for i in range(len(details['question'])):
-            question = sanitize_html(details['question'][i])
-            answer_score = details['answer_score'][i]
-            answer = sanitize_html(details['answer'][i])
-            question_tags = sanitize_html(details['question_tags'][i])
+        story.append(Paragraph(f"<b>Month: {month}</b>", styles["Heading2"]))
 
-            # Render text as HTML-compatible paragraphs
-            story.append(Paragraph(f"<b>Question {i + 1}:</b>", styles['BodyText']))
-            story.append(Paragraph(f"<b>Tags:</b> {question_tags}", styles['BodyText']))
-            story.append(Paragraph(f"<b>Question:</b> {question}", styles['BodyText']))
-            story.append(Paragraph(f"<b>Answer Score:</b> {answer_score}", styles['BodyText']))
-            story.append(Paragraph(f"<b>Answer:</b> {answer}", styles['BodyText']))
-            story.append(Spacer(1, 12))  # Add space between entries
+        for i in range(len(details["question"])):
+            question = sanitize_html(details["question"][i])
+            answer_score = details["answer_score"][i]
+            answer = sanitize_html(details["answer"][i])
+            question_tags = sanitize_html(details["question_tags"][i])
 
-    # Build the PDF
+            story.append(Paragraph(f"<b>Question {i + 1}:</b>", styles["BodyText"]))
+            story.append(Paragraph(f"<b>Tags:</b> {question_tags}", styles["BodyText"]))
+            story.append(Paragraph(f"<b>Question:</b> {question}", styles["BodyText"]))
+            story.append(
+                Paragraph(f"<b>Answer Score:</b> {answer_score}", styles["BodyText"])
+            )
+            story.append(Paragraph(f"<b>Answer:</b> {answer}", styles["BodyText"]))
+            story.append(Spacer(1, 12))
+
     doc.build(story)
     print("PDF report generated successfully!")
-    
-    
 
 
 def sanitize_html(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    
-    # Remove all <img> tags
-    for img_tag in soup.find_all('img'):
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    for img_tag in soup.find_all("img"):
         img_tag.decompose()
-    
-    # Remove all <sub> tags
-    for sub_tag in soup.find_all('sub'):
+
+    for sub_tag in soup.find_all("sub"):
         sub_tag.decompose()
-    
-    # Remove unsupported attributes like 'rel' and 'title' from <a> tags
+
     for tag in soup.find_all(True):
-        if tag.name == 'a':
-            unsupported_attrs = ['rel', 'title']
+        if tag.name == "a":
+            unsupported_attrs = ["rel", "title"]
             for attr in unsupported_attrs:
                 if attr in tag.attrs:
                     del tag.attrs[attr]
 
-    # Replace <br> tags with newline characters
-    for br_tag in soup.find_all('br'):
-        br_tag.replace_with('\n')
-    
+    for br_tag in soup.find_all("br"):
+        br_tag.replace_with("\n")
+
     return str(soup)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parse_code()
